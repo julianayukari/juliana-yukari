7c18a7ce89c97acaa59d6bda8e7e8bad795755c4
Juliana Yukari <julianayukari36@gmail.com>
2019-06-21 03:07:47 -0300

    Updates

---
+++ precache-manifest.8a71a92deb41fbc52286f3fafae73957.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4faaf5c4571e0d5ac1ee8ec017e9cd77",
+    "url": "/juliana-yukari/index.html"
+  },
+  {
+    "revision": "43fbff13698523481392",
+    "url": "/juliana-yukari/static/css/main.2e0e255d.chunk.css"
+  },
+  {
+    "revision": "70bd9d20d378222aefe8",
+    "url": "/juliana-yukari/static/js/2.6ae588c1.chunk.js"
+  },
+  {
+    "revision": "43fbff13698523481392",
+    "url": "/juliana-yukari/static/js/main.897cdff6.chunk.js"
+  },
+  {
+    "revision": "b1f68d6ab2bdb036a2eb",
+    "url": "/juliana-yukari/static/js/runtime~main.789253d7.js"
+  },
+  {
+    "revision": "d713ac7da09736caa388012bf673ec41",
+    "url": "/juliana-yukari/static/media/Resume_Juliana.d713ac7d.pdf"
+  },
+  {
+    "revision": "a231c69a9071bfdcd079b4c884d29387",
+    "url": "/juliana-yukari/static/media/bg2.a231c69a.jpg"
+  },
+  {
+    "revision": "1d458824d124c33f8d71101a4a175141",
+    "url": "/juliana-yukari/static/media/bg4.1d458824.jpg"
+  },
+  {
+    "revision": "fc92ebd7c53389e91d1c3ee767fe4b0f",
+    "url": "/juliana-yukari/static/media/bulb.fc92ebd7.png"
+  },
+  {
+    "revision": "3d6637d518ab650e86d1c7cdde9b0d42",
+    "url": "/juliana-yukari/static/media/img1.3d6637d5.jpeg"
+  },
+  {
+    "revision": "f9b68cb53265da26992a4117908802b2",
+    "url": "/juliana-yukari/static/media/img2.f9b68cb5.jpeg"
+  },
+  {
+    "revision": "0c7ccdf6217ee5dfe2be3c0ea19a8d3a",
+    "url": "/juliana-yukari/static/media/img3.0c7ccdf6.jpg"
+  },
+  {
+    "revision": "43d67fe40f2c119b290865ba2bdd9704",
+    "url": "/juliana-yukari/static/media/juliana.43d67fe4.png"
+  },
+  {
+    "revision": "970d021406d7ce5823a7f37cfc3e31f6",
+    "url": "/juliana-yukari/static/media/sicov.970d0214.png"
+  },
+  {
+    "revision": "a2562566f7dfcc1da99d149798234c2c",
+    "url": "/juliana-yukari/static/media/site.a2562566.png"
+  }
+]);
